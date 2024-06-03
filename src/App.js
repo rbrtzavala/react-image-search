4b10deb124a875f182e4cedb8137caf79f1e16f4
Robert Zavala <rbrtzavala@gmail.com>
2024-06-03 15:43:03 -0600

    wire up App to search for images from the submit event on the SearchBar component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-
 import ImageList from "./components/ImageList.js";
 import SearchBar from "./components/SearchBar.js";
+import searchImages from "./api.js";
 
 function App() {
+  const handleSubmit = (term) => {
+    console.log(`Do a search for: ${term}`);
+    searchImages(term);
+  }
+
   return (
     <div>
-      <h1>
-        App
-      </h1>
-      <SearchBar />
+      <SearchBar onSubmit={handleSubmit} />
       <ImageList />
     </div>
   )
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,32 @@
-function SearchBar() {
-  return <h2>SearchBar</h2>
+import { useState } from 'react';
+
+function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState('');
+
+  const handleChange = (event) => {
+    let value = event.target.value
+
+    setTerm(value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    onSubmit(term);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          placeholder='What are you searching for?'
+          value={term}
+          onChange={handleChange}
+        />
+      </form>
+    </div>
+  )
 }
 
 export default SearchBar;
